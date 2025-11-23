a3f42119fe5d47eb44793a4ed0d6335e701a0c24
mahmoud-sayed33 <ma-sayedmohamed@elarabygroup.com>
2025-11-23 20:50:33 +0200

    adding: adding the initial dependencies

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Linux Device Drivers
 sudo apt-get update
 sudo apt-get install build-essential lzop u-boot-tools net-tools bison flex libssl-dev libncurses5-dev libncursesw5-dev unzip chrpath xz-utils minicom wget git-core
 ```
+
+## Download the linux kernel from the following URL:
+```c
+https://github.com/raspberrypi/linux
+```
+
+## Download the toolchain from the following URL:
+```c
+https://snapshots.linaro.org/gnu-toolchain/14.0-2023.06-1/arm-linux-gnueabihf/
+```
---
+++ toolchain_path.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=$PATH:/home/mahmoud/Desktop/ATON/Linux_Device_Drivers/Linux-Device-Drivers/Linux-System-programming/Downloads/cross-gcc-12.2.0-pi_3+/cross-pi-gcc-12.2.0-2/bin
+export PATH=$PATH:/home/mahmoud/Desktop/ATON/Linux_Device_Drivers/Linux-Device-Drivers/Linux-System-programming/Downloads/cross-gcc-12.2.0-pi_64/cross-pi-gcc-12.2.0-64/bin
+export PATH=$PATH:/home/mahmoud/Desktop/ATON/Linux_Device_Drivers/Linux-Device-Drivers/Linux-System-programming/Downloads/gcc-linaro-14.0.0-2023.06-aarch64_arm-linux-gnueabihf/bin
